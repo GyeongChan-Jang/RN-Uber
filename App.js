f5b3e867c9e9bbd88f2ff3a432c03116bf85c163
chan <cham9994@gmail.com>
2022-11-04 22:29:48 +0900

    feat: 리액트 네이티브 네비게이션 적용

--- App.js
+++ App.js
@@ -2,13 +2,36 @@ import { StyleSheet, Text, View } from 'react-native'
 import { Provider } from 'react-redux'
 import HomeScreen from './screens/HomeScreen'
 import { store } from './store'
-
-// 1) Set up redux
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import MapScreen from './screens/MapScreen'
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
+
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                headerShown: false
+              }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   )
 }
